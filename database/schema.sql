fb36e5f65abd8cfcca3b8bcee1e14ebfee00397f
sorry-im-got-nervous <valeriapashkova13@gmail.com>
2026-07-03 21:12:16 +0800

    Добавлены тестовые данные для БД

--- database/schema.sql
+++ database/schema.sql
@@ -1,25 +1,64 @@
-//Код для создания бд
+--
+-- Структура таблицы `users`
+--
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+
     full_name VARCHAR(100) NOT NULL,
+
     email VARCHAR(100) NOT NULL UNIQUE,
+
     password_hash VARCHAR(255) NOT NULL,
-    role ENUM('user', 'admin') DEFAULT 'user',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+    role ENUM('user', 'admin')
+        DEFAULT 'user',
+
+    created_at TIMESTAMP
+        DEFAULT CURRENT_TIMESTAMP
 );
+
+-- --------------------------------------------------------
+
+
+--
+-- Структура таблицы `resources`
+--
 
 CREATE TABLE resources (
     id INT AUTO_INCREMENT PRIMARY KEY,
+
     name VARCHAR(100) NOT NULL UNIQUE,
+
     description TEXT,
-    type VARCHAR(50),
-    responsible_user_id INT NULL,
-    status ENUM('active', 'inactive') DEFAULT 'active',
+
+    type ENUM(
+        'room',
+        'equipment',
+        'employee',
+        'vehicle',
+        'other'
+    ) NOT NULL,
+
+    responsible_user_id INT,
+
+    status ENUM('active', 'inactive')
+        DEFAULT 'active',
+
+    INDEX (responsible_user_id),
 
     FOREIGN KEY (responsible_user_id)
-    REFERENCES users(id)
-    ON DELETE SET NULL
+        REFERENCES users(id)
+        ON DELETE SET NULL
 );
+
+-- --------------------------------------------------------
+
+
+
+--
+-- Структура таблицы `bookings`
+--
 
 CREATE TABLE bookings (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -35,18 +74,30 @@ CREATE TABLE bookings (
     purpose VARCHAR(255),
 
     status ENUM('active', 'cancelled')
-    DEFAULT 'active',
+        DEFAULT 'active',
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP
+        DEFAULT CURRENT_TIMESTAMP,
+
+    INDEX (resource_id),
+    INDEX (user_id),
 
     FOREIGN KEY (resource_id)
         REFERENCES resources(id)
-        ON DELETE CASCADE,
+        ON DELETE RESTRICT,
 
     FOREIGN KEY (user_id)
         REFERENCES users(id)
         ON DELETE CASCADE
 );
+
+-- --------------------------------------------------------
+
+
+
+--
+-- Структура таблицы `notifications`
+--
 
 CREATE TABLE notifications (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -57,9 +108,13 @@ CREATE TABLE notifications (
 
     message TEXT,
 
-    is_read BOOLEAN DEFAULT FALSE,
+    is_read BOOLEAN
+        DEFAULT FALSE,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP
+        DEFAULT CURRENT_TIMESTAMP,
+
+    INDEX (user_id),
 
     FOREIGN KEY (user_id)
         REFERENCES users(id)
